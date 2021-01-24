2921a7b5999d263dcfacdbaf66c378b10d1183bd
Hanqing1996 <zhq199622@163.com>
2021-01-24 20:32:20 +0800

    实现以 index.js 为起点收集模块依赖

--- bundle.js
+++ bundle.js
@@ -1,8 +1,66 @@
 const fs = require('fs')
-let fileContent = fs.readFileSync('./src/index.js', 'utf-8');
+const path=require('path')
 
-const indexFunction=()=>{
-    eval(`${fileContent}`)
+
+
+function getFileContentByPath(filePath) {
+    const fileContent=fs.readFileSync(filePath, 'utf-8');
+    return fileContent
 }
 
-indexFunction();
+function getFileDependencies(fileContent) {
+    let reg = /require\(['"](.+?)['"]\)/g;
+    let result = null;
+    let dependencies = [];
+    while(result = reg.exec(fileContent)) {
+        dependencies.push(result[1]);
+    }
+    return dependencies;
+}
+
+let ID = 0;
+
+function createAsset(absolutePath) {
+    
+    const fileContent=getFileContentByPath(absolutePath)
+    return{
+        path:absolutePath,
+        id:ID++,
+        code:(require)=>eval(`${fileContent}`),
+        dependencies:getFileDependencies(fileContent)
+    }
+}
+
+
+
+function createGraph(enterPath) {
+    const EnterAsset=createAsset(enterPath)
+    const queue=[EnterAsset]
+    
+    // 以 EnterAsset 为起点，收集模块依赖,令 queue 不断增长
+    for(let asset of queue){
+        const dirName=path.dirname(asset.path)
+        asset.dependencies.forEach(relativePath=>{
+            const absolutePath=path.join(dirName,relativePath)
+            console.log(absolutePath);
+            const newAsset=createAsset(absolutePath)
+            queue.push(newAsset)
+        })
+    }
+    return queue
+}
+
+
+const queue=createGraph('./src/index.js')
+console.log(queue);
+
+
+
+
+
+
+
+
+
+
+
